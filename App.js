ae884e9c25a44732372f0a2688f1972adb34aa0c
Jacksonblair <jackson.blair@live.com>
2021-01-28 18:30:43 +1100

    fix

--- App.js
+++ App.js
@@ -11,9 +11,9 @@ app.use(function(req, res, next) {
 
 	console.log(req)
 
-	res.header("Access-Control-Allow-Origin", "*")
+	res.header("Access-Control-Allow-Origin", "https://jackson-blair-react-crud-demo.herokuapp.com")
 	res.header('Access-Control-Allow-Credentials', true)
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Headers')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
 	next()
 })
