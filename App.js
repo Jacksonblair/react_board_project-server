b0317af7ddfdfc817bea451c1698835e9478e809
Jacksonblair <jackson.blair@live.com>
2021-01-28 18:44:18 +1100

    cors

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
 
 	res.header("Access-Control-Allow-Origin", "https://jackson-blair-react-crud-demo.herokuapp.com")
 	res.header('Access-Control-Allow-Credentials', true)
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, content-type, Accept, Authorization')
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
 	res.header('Access-Control-Max-Age', 86400)
 	next()
