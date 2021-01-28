28fa7eb37a48e602863b063f5120849d9733e55f
Jacksonblair <jackson.blair@live.com>
2021-01-28 18:38:55 +1100

    pls work

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Credentials', true)
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+	res.header('Access-Control-Max-Age', 86400)
 	next()
 })
 
