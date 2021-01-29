b821c5d75346696e3e09fdd57e6356e828759936
Jacksonblair <jackson.blair@live.com>
2021-01-29 11:17:27 +1100

    help

--- API/auth/index.js
+++ API/auth/index.js
@@ -10,7 +10,7 @@ exports.addJwtToCookie = (res, user) => {
 			profile_image_url: user.profile_image_url	
 		})
 	}, process.env.SECRET)
-	res.cookie('jwt', token, { httpOnly: true, sameSite: "none" })
+	res.cookie('jwt', token, { httpOnly: true, sameSite: "None", secure: true })
 	res.setHeader('Content-Type', 'application/json');
 }
 
@@ -24,7 +24,8 @@ exports.addUserDetailsToCookie = (res, user) => {
 	}),
 	{ // JSON response must be encoded as a string
 		encode: String,
-		sameSite: "none"
+		sameSite: "None",
+		secure: true
 	})
 }
 
