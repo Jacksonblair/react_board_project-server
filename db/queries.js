9a4d94fd82a205d35abcb9c313f5bb7db711608a
Jacksonblair <jackson.blair@live.com>
2021-02-07 17:01:55 +1100

    fix queries

--- db/queries.js
+++ db/queries.js
@@ -20,12 +20,14 @@ exports.GET_POST_BY_POST_ID = `SELECT id, board_id, created_at, created_by_user_
 								to_char(target_date, 'TZ') AS target_timezone
 								FROM posts WHERE id = $1`
 
-exports.ADD_NEW_POST = "INSERT INTO posts (title, content, target_date, created_by_user_id, board_id) VALUES ($1, $2, $3, $4, $5)"
+exports.ADD_NEW_POST = "INSERT INTO posts (title, content, target_date, created_by_user_id, board_id) VALUES ($1, $2, TO_DATE($3, 'dd/mm/yyyy'), $4, $5)"
+
 exports.DELETE_POST_BY_POST_ID = "DELETE FROM posts WHERE id = $1 RETURNING *"
 exports.DELETE_BOARD_BY_BOARD_ID = "DELETE FROM boards WHERE id = $1 RETURNING *"
-exports.UPDATE_POST_BY_POST_ID = "UPDATE posts SET title = $1, content = $2, target_date = $3 WHERE id = $4 RETURNING *"
+exports.UPDATE_POST_BY_POST_ID = "UPDATE posts SET title = $1, content = $2, target_date = TO_DATE($3, 'dd/mm/yyyy') WHERE id = $4 RETURNING *"
 exports.UPDATE_BOARD_BY_BOARD_ID = "UPDATE boards SET name = $1, description = $2, public = $3 WHERE id = $4 RETURNING *"
 exports.GET_USER_BY_USER_ID = "SELECT * FROM users WHERE id = $1"
 exports.UPDATE_USER_EMAIL_BY_USER_ID = "UPDATE users SET email = $1 WHERE id = $2 RETURNING *"
 exports.UPDATE_USER_PASSWORD_BY_USER_ID = "UPDATE users SET password_hash = $1 WHERE id = $2 RETURNING *"
 exports.CLEAR_GUEST_BOARD_DATA = `DELETE FROM boards WHERE created_by_user_id = ${process.env.GUEST_ACCOUNT_ID}`
+
